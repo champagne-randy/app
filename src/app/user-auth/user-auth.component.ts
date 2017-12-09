cde75c040daa81d523b73c0097bcb1a5cc441040
Randy Champagne <champagne.randy@gmail.com>
2017-12-09 02:10:08 -0500

    mv auth markup to UserAuthComponent

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+	Component, 
+	OnInit 
+} 					from '@angular/core';
 
 @Component({
-  selector: 'app-user-auth',
-  templateUrl: './user-auth.component.html',
-  styleUrls: ['./user-auth.component.css']
+	selector: 'app-user-auth',
+	templateUrl: './user-auth.component.html',
+	styleUrls: ['./user-auth.component.css']
 })
 export class UserAuthComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
