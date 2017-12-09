7da6f5a59be69958bc920012dc41b9e23bf5d2f4
Randy Champagne <champagne.randy@gmail.com>
2017-12-09 01:26:26 -0500

    minor formating update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,37 @@
-import { NgModule }				from '@angular/core';
+import { 
+	NgModule 
+}								from '@angular/core';
 import { 
 	RouterModule, 
 	Routes 
 } 								from '@angular/router';
 
-import { FileUploadComponent }	from './file-upload/file-upload.component';
-import { MindMapComponent }		from './mind-map/mind-map.component'
+import { 
+	FileUploadComponent 
+}								from './file-upload/file-upload.component';
+import { 
+	MindMapComponent 
+}								from './mind-map/mind-map.component'
 
 
 
 const routes: Routes = [
 	// paths
-	{ path: 'upload',  	component: FileUploadComponent },
-	{ path: 'map',  	component: MindMapComponent },
+	{ 
+		path: 'upload',  	
+		component: FileUploadComponent 
+	},
+	{ 
+		path: 'map',  	
+		component: MindMapComponent 
+	},
 
 	// redirects
-	{ path: '', redirectTo: '/upload', pathMatch: 'full' },
+	{ 
+		path: '', 
+		redirectTo: '/upload', 
+		pathMatch: 'full' 
+	},
 ];
 
 
